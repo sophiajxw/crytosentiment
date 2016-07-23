114cb6329d8f93ffda45d87644ac1aa2f4c8b291
sophiajiang <sophiajxw@gmail.com>
2016-07-22 22:06:37 -0400

    basic done

--- src/components/note.js
+++ src/components/note.js
@@ -15,14 +15,23 @@ class Note extends Component {
     this.onDelete = this.onDelete.bind(this);
     this.onUpdate = this.onUpdate.bind(this);
     this.onDrag = this.onDrag.bind(this);
+    this.onEditRender = this.onEditRender.bind(this);
+    this.onTextRender = this.onTextRender.bind(this);
   }
 
-  onEdit(event) {
+  onEdit() {
     if (this.state.isEditing) {
-      this.setState({ isEditing: true });
-      return <div>editing!</div>;
-    } else {
       this.setState({ isEditing: false });
+    } else {
+      this.setState({ isEditing: true });
+    }
+  }
+
+  onEditRender() {
+    if (this.state.isEditing) {
+      return <i className="fa fa-pencil-square-o" onClick={this.onEdit} />;
+    } else {
+      return <i className="fa fa-check-square-o" onClick={this.onEdit} />;
     }
   }
 
@@ -32,6 +41,18 @@ class Note extends Component {
 
   onUpdate(event) {
     this.props.updateNote(this.props.id, { text: event.target.value });
+  }
+
+  onTextRender() {
+    if (this.state.isEditing) {
+      return (
+        <textarea id="text" value={this.props.note.text} onChange={this.onUpdate} />
+      );
+    } else {
+      return (
+        <div id="greyarea" dangerouslySetInnerHTML={{ __html: marked(this.props.note.text || '') }} />
+      );
+    }
   }
 
   onDrag(event, ui) {
@@ -50,13 +71,13 @@ class Note extends Component {
         onStop={this.onStopDrag}
       >
         <div>
-          <div>
+          <div className="note">
             <span id="note_title">{this.props.note.title}</span>
             <i id="note_item" onClick={this.onDelete} className="fa fa-trash-o" />
-            <i id="note_item" type="checkbox" onClick={this.onEdit} className="fa fa-pencil-square-o" aria-hidden="true" />
+            {this.onEditRender()}
             <i id="note_item" className="fa fa-arrows-alt note-mover" aria-hidden="true"></i>
           </div>
-          <textarea id="text" value={this.props.note.text} onChange={this.onUpdate} />
+          {this.onTextRender()}
         </div>
       </Draggable>
     );
