0e803550d776a3973e43efcbebd35b078c6e1b11
sophiajiang <sophiajxw@gmail.com>
2018-03-03 23:08:20 -0500

    d

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -21,17 +21,66 @@ class LineChart extends Component {
     return data.reduce((max, p) => p.y > max ? p.y : max, data[0].y);
   }
 
-  render() {
+  getSvgX(x) {
+    const { svgWidth } = this.props;
+    return (x / this.getMaxX() * svgWidth);
+  }
+  getSvgY(y) {
+    const { svgHeight } = this.props;
+    return svgHeight - (y / this.getMaxY() * svgHeight);
+  }
+
+  makePath() {
+    const { data, color } = this.props;
+    let pathD = 'M ' + this.getSvgX(data[0].x) + ' ' + this.getSvgY(data[0].y) + ' ';
+    pathD += data.map((point, i) => {
+      return 'L ' + this.getSvgX(point.x) + ' ' + this.getSvgY(point.y) + ' ';
+    });
     return (
-      <svg>
+      <path className="linechart_path" d={pathD} style={{ stroke: color }} />
+    );
+  }
+
+  makeAxis() {
+    const minX = this.getMinX();
+    const maxX = this.getMaxX();
+    const minY = this.getMinY();
+    const maxY = this.getMaxY();
+
+    console.log(minX);
+    console.log(maxX);
+    console.log(minY);
+    console.log(maxY);
+
+    return (
+      <g className="linechart_axis">
+        <line
+          x1={this.getSvgX(minX)} y1={this.getSvgY(minY)}
+          x2={this.getSvgX(maxX)} y2={this.getSvgY(minY)}
+        />
+        <line
+          x1={this.getSvgX(minX)} y1={this.getSvgY(minY)}
+          x2={this.getSvgX(minX)} y2={this.getSvgY(maxY)}
+        />
+      </g>
+    );
+  }
+
+  render() {
+    const { svgHeight, svgWidth } = this.props;
+    return (
+      <svg viewBox={`0 0 ${svgWidth} ${svgHeight}`}>
+        {this.makePath()}
+        {this.makeAxis()}
       </svg>
     );
   }
 }
 LineChart.defaultProps = {
   data: [],
-  color: '#2196F3',
-  svgHeight: 300,
-  svgWidth: 700,
+  color: '#f22183',
+  svgHeight: 500,
+  svgWidth: 500,
 };
+
 export default LineChart;
